45596591d5cd9ef73082d04a4062a6a8f7436ecc
om909ar <139689383+om909ar@users.noreply.github.com>
2025-04-10 18:17:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ import App from './App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';  // App.js is the main file that holds your components
+
+ReactDOM.render(<App />, document.getElementById('root'));
