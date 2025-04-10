bda34bef5384535cafefa421c54194601d04fb1b
om909ar <139689383+om909ar@users.noreply.github.com>
2025-04-10 19:45:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';  // تأكد من أن App.js موجود
+import App from './App';  // تأكد من أن App.js موجود في نفس المجلد
 
-// تحديد المكان الذي سيتم تحميل التطبيق فيه
+// تحديد العنصر الذي سيتم تحميل التطبيق فيه
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
